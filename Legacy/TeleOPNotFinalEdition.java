6cbd0478853d2eef2e81db3b929226cfd78e42e8
lightningblade5678 <lightningblade5678@gmail.com>
2019-10-02 16:23:34 -0400

    Update TeleOPNotFinalEdition.java
    Revert

--- Legacy/TeleOPNotFinalEdition.java
+++ Legacy/TeleOPNotFinalEdition.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc;
 //movement
 
-//test123 yeet
-
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
