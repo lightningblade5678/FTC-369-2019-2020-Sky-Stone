2e40376308129c205c70a1db374973129f62501b
Carson Harter <carsonharter@gmail.com>
2019-10-07 19:33:10 -0400

    Revert "new MotorTest"
    This reverts commit fd65c625c106face48c1c95e003e8b529e0d545a, reversing
    changes made to 3c9e1a0be129d147eeb9b0cf5d15a196691ea0b6.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -40,50 +39,3 @@ public class MotorTest extends LinearOpMode {
         backRight.setPower(0);
     }
 }
-=======
-package org.firstinspires.ftc;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-@Autonomous(name = "motorTest")
-
-public class MotorTest extends LinearOpMode {
-
-    public DcMotor backLeft;
-    public DcMotor backRight;
-    public DcMotor frontLeft;
-    public DcMotor frontRight;
-
-    @Override
-    //testing motors
-    public void runOpMode () {
-
-        backleft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-
-        waitForStart();
-        while (opModeIsActive()) {
-            testMotors.leftBack.setPower(1);
-            testMotors.rightBack.setPower(1);
-            testMotors.leftFront.setPower(1);
-            testMotors.rightFront.setPower(1);
-
-            // Let the motors run for 4 seconds
-            sleep(4000);
-
-            testMotors.leftBack.setPower(0);
-            testMotors.rightBack.setPower(0);
-            testMotors.leftFront.setPower(0);
-            testMotors.rightFront.setPower(0);
-        }
-    }
-
-
-    }
->>>>>>> 74de945c4d136d566cb60e2a61e9e7141b7cb7bc
