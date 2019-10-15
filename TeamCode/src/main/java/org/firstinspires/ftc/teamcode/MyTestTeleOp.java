6666e6f3637bef4dbdd3de1fe357c76478614631
lightningblade5678 <lightningblade5678@gmail.com>
2019-10-14 20:04:01 -0400

    Test Teleop, can ignore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTestTeleOp.java
@@ -12,21 +12,32 @@ import org.firstinspires.ftc.teamcode.FinalBot.FinalBot;
 @TeleOp
 public class MyTestTeleOp extends OpMode {
 
+    FinalBot test;
+
     private BotWheels wheels = new BotWheels(hardwareMap.dcMotor.get("frontLeft"),hardwareMap.dcMotor.get("frontRight"),
                                hardwareMap.dcMotor.get("backLeft"),hardwareMap.dcMotor.get("backRight"));
     private BotIntake intake = new BotIntake(hardwareMap.dcMotor.get("intakeLeft"),hardwareMap.dcMotor.get("intakeRight"),
                                 hardwareMap.touchSensor.get(""));
-    private BotArm arm;
+    private BotArm arm = new BotArm(new DcMotor []{hardwareMap.dcMotor.get("swing")});
 
 
     public void init(){
 
-        FinalBot test = new FinalBot(wheels,intake,arm);
+        telemetry.addData("Status", "Initializing...");
+
+        test = new FinalBot(wheels,intake,arm);
+
+        telemetry.addData("Status", "Initialized");
+
 
     }
 
     public void loop(){
 
+        double power;
+        double turn;
+
+        double force = g
 
     }
 
