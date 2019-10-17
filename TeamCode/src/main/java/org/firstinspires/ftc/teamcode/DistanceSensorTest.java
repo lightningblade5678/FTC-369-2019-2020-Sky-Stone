4e17edc8adfaefce7a4b6d9d75ef20715593f8ec
Eric Yung <32915329+Nanomerce@users.noreply.github.com>
2019-10-16 20:13:05 -0400

    made distancesensor work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -17,7 +18,7 @@ import java.util.LinkedList;
 @Autonomous(name = "DistanceSensorTest")
 public class DistanceSensorTest extends LinearOpMode {
 
-    DistanceSensor dist = null;
+    ModernRoboticsI2cRangeSensor dist = null;
     LinkedList<DcMotor> motors = null;
 
     private ElapsedTime passTime = new ElapsedTime(0);//"clock" to keep track of passed time
@@ -27,28 +28,38 @@ public class DistanceSensorTest extends LinearOpMode {
 
         try{
 
-            dist = hardwareMap.getAll(DistanceSensor.class).get(0);
+            dist = (ModernRoboticsI2cRangeSensor) hardwareMap.get("range");
             motors = (LinkedList<DcMotor>) hardwareMap.getAll(DcMotor.class);
 
-        }catch (NullPointerException e){
+        }catch (Exception e){
+            telemetry.addData("ERROR: ","Section 1");
+            telemetry.addData("E: ",e);
+            telemetry.update();
 
+            sleep(30000);
         }
 
-        passTime.reset();
-        while(passTime.seconds() < 30){
+        try {
 
-            if(dist.getDistance(DistanceUnit.INCH) < 1){
-                for(int i = 0; i < motors.size(); i++){
-                    motors.get(i).setPower(1);
+            passTime.reset();
+            while (passTime.seconds() < 30) {
+
+                if (dist.getDistance(DistanceUnit.INCH) < 1) {
+                    for (int i = 0; i < motors.size(); i++) {
+                        motors.get(i).setPower(1);
+                    }
+                } else {
+                    for (int i = 0; i < motors.size(); i++) {
+                        motors.get(i).setPower(0);
+                    }
                 }
-            }else{
-                for(int i = 0; i < motors.size(); i++){
-                    motors.get(i).setPower(0);
-                }
-            }
 
-        }//runs for 30 seconds
-
+            }//runs for 30 seconds
+        }catch(Exception e){
+            telemetry.addData("ERROR: ","Section 1");
+            telemetry.addData("E: ",e);
+            telemetry.update();
+        }
     }
 
 }
