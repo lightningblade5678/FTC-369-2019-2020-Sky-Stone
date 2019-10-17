0605311d6791fa766865d6076bcf3968da29307d
lightningblade5678 <lightningblade5678@gmail.com>
2019-10-16 20:15:52 -0400

    TeleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTestTeleOp.java
@@ -1,45 +1,115 @@
-/*package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.FinalBot.BotArm;
-import org.firstinspires.ftc.teamcode.FinalBot.BotIntake;
-import org.firstinspires.ftc.teamcode.FinalBot.BotWheels;
-import org.firstinspires.ftc.teamcode.FinalBot.FinalBot;
 
 @TeleOp
 public class MyTestTeleOp extends OpMode {
 
-    FinalBot test;
+    public DcMotor frontLeft;
+    public DcMotor backLeft;
+    public DcMotor frontRight;
+    public DcMotor backRight;
 
-    private BotWheels wheels = new BotWheels(hardwareMap.dcMotor.get("frontLeft"),hardwareMap.dcMotor.get("frontRight"),
-                               hardwareMap.dcMotor.get("backLeft"),hardwareMap.dcMotor.get("backRight"));
-    //private BotIntake intake = new BotIntake(hardwareMap.dcMotor.get("intakeLeft"),hardwareMap.dcMotor.get("intakeRight"),
-                                //hardwareMap.touchSensor.get("hmmm"));
-    private BotArm arm = new BotArm(new DcMotor []{hardwareMap.dcMotor.get("swing")});
+    public DcMotor intakeMotorRight;
+    public DcMotor intakeMotorLeft;
 
+    @Override
 
     public void init(){
 
-        telemetry.addData("Status", "Initializing...");
+        telemetry.addData("Status", "Initializing");
 
-        //test = new FinalBot(wheels,intake,arm);
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        intakeMotorRight = hardwareMap.get(DcMotor.class, "inRight");
+        intakeMotorLeft = hardwareMap.get(DcMotor.class, "inLeft");
+
 
         telemetry.addData("Status", "Initialized");
-
 
     }
 
     public void loop(){
 
-        double power;
-        double turn;
+        if(gamepad1.right_stick_y == 1){
 
-        double force;
+            telemetry.addData("Direction","Forwards");
+
+            allMotors(1);
+
+        }
+        if(gamepad1.right_stick_y == -1){
+
+            telemetry.addData("Direction","Backwards");
+
+            allMotors(-1);
+
+        }
+
+        if(gamepad1.right_stick_x == -1){
+
+            telemetry.addData("Direction","Strafe Left");
+
+            frontLeft.setPower(-1);
+            frontRight.setPower(1);
+            backLeft.setPower(1);
+            backRight.setPower(-1);
+
+        }
+
+        if(gamepad1.right_stick_x == 1){
+
+            telemetry.addData("Direction","Strafe Right");
+
+            frontLeft.setPower(1);
+            frontRight.setPower(-1);
+            backLeft.setPower(-1);
+            backRight.setPower(1);
+
+        }
+
+        if(gamepad1.left_stick_x == 1 || gamepad1.left_stick_y == 1){
+
+            frontLeft.setPower(-1);
+            frontRight.setPower(1);
+            backLeft.setPower(-1);
+            backRight.setPower(1);
+
+        }
+
+        if(gamepad1.left_stick_x == -1 || gamepad1.left_stick_y == -1){
+
+            frontLeft.setPower(1);
+            frontRight.setPower(-1);
+            backLeft.setPower(1);
+            backRight.setPower(-1);
+
+        }
+
+        if(gamepad2.right_trigger == 1){
+
+            double x = -1;
+
+            //intakeMotorLeft.intakeStart();
+            intakeMotorRight.setPower(-1*x);
+
+        }
+
+    }
+
+    public void allMotors(double x){
+
+        frontLeft.setPower(x);
+        frontRight.setPower(x);
+        backLeft.setPower(x);
+        backRight.setPower(x);
 
     }
 
 }
-*/
