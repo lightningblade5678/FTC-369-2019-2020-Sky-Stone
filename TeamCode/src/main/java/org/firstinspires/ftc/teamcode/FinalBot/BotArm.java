4dd5a509723e4cf54f97d735b88ff55e91a16b10
Carson Harter <carsonharter@gmail.com>
2019-10-30 18:11:29 -0400

    Revert "Arm support code"
    This reverts commit 0fa6f6f82d22fcf171388e6197b736140acd2488.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/BotArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/BotArm.java
@@ -9,14 +9,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class BotArm {
 
-    //vars for encoders
-
-    private static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder is 1440, neverest classic 40 are 1120
-    private static final double     DRIVE_GEAR_REDUCTION    =  1;     // This is < 1.0 if geared UP
-
-    //end encoder vars
-
-
     public DcMotor baseMotor;
     public Servo wristServo;
     public Servo handServo;
@@ -57,25 +49,5 @@ public class BotArm {
     }
 
     }//detects if hand is closed or not and inverts results
-
-    public void rotateDegree(DcMotor armMotor, double degrees, double power){
-        DcMotor.RunMode temp = armMotor.getMode();//saves runmode of motor
-        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);//sets mode to use encoder
-
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);//begins runmode
-
-        double target = armMotor.getCurrentPosition()+( (degrees/360) *COUNTS_PER_MOTOR_REV*DRIVE_GEAR_REDUCTION);//sets target to # of rotations and converts that to motor counts
-
-        armMotor.setPower(power);//starts motor at desired power
-
-        while(armMotor.isBusy());//waits until arm is done
-
-        armMotor.setPower(0);//stops motor
-
-        if(Math.abs(armMotor.getCurrentPosition()-target) > COUNTS_PER_MOTOR_REV*0.1){//10% error threshold
-            rotateDegree(armMotor, (target-armMotor.getCurrentPosition())/COUNTS_PER_MOTOR_REV/DRIVE_GEAR_REDUCTION*360 , power/2  );//corrects error at slower speed
-        }//error correction
-
-    }//rotates motor # of degrees
 
 }
