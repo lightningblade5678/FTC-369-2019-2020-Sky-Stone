3d2eb29de30dd702651ee3e870945686ca9a12f1
Eric Yung <32915329+Nanomerce@users.noreply.github.com>
2019-11-04 18:25:12 -0500

    Update GetArmPosition.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/GetArmPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/GetArmPosition.java
@@ -12,9 +12,9 @@ public class GetArmPosition extends LinearOpMode {
     @Override
     public void runOpMode(){
 
-        Servo wrist = hardwareMap.get(Servo.class, "wrist");//gets wrist
+        Servo wrist = hardwareMap.get(Servo.class, "wristServo");//gets wrist
 
-        DcMotor arm = hardwareMap.get(DcMotor.class, "arm");//gets arm
+        DcMotor arm = hardwareMap.get(DcMotor.class, "baseMotor");//gets arm
 
         telemetry.addData("Wrist",wrist.getPosition());
         telemetry.addData("Arm",arm.getCurrentPosition());
