89e4e4df8eeb83b6a178ff7df02d65e71ba99058
Eric Yung <32915329+Nanomerce@users.noreply.github.com>
2019-11-13 19:14:10 -0500

    Replaced encoders in arm with time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/BotArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/BotArm.java
@@ -14,16 +14,11 @@ package org.firstinspires.ftc.teamcode.FinalBot;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class BotArm {
 
-    //vars for encoders
-
-    private static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder is 1440, neverest classic 40 are 1120
-    private static final double     DRIVE_GEAR_REDUCTION    =  120/80;     // This is < 1.0 if geared UP
-
-    //end encoder vars
-
+    private static final double rpm = 152;
 
     public DcMotor baseMotor;
     public Servo wristServo;
@@ -37,9 +32,9 @@ public class BotArm {
 
     //start work method
 
-    public void rotateWrist(double wristDegree){
+    public void rotateWrist(double wristDegree /*never set this above 180 or below 0*/ ){
         wristServo.setPosition(wristDegree/180); // [!] Check params
-    }//sets servo to degrees between 180
+    }//sets servo to degrees between 0-180
 
 
     public void handGrab(boolean state){
@@ -48,49 +43,22 @@ public class BotArm {
         }
         else{
             handServo.setPosition(1); // [!] check parameter
-        }
-
+        }//closes hand
 
     }//detects if hand is closed or not and inverts results
 
-    public void baseRotateDegree(DcMotor armMotor, double degrees, double power){
+    public void baseRotateDegree(DcMotor base, double deg, double speed /*Never set this below 0 or above 1*/ ){
+        
+        double timeRot /*in seconds*/= deg/ ( (rpm*speed)/60*360 );//calculates time that the arm needs to rotate for
 
-        DcMotor.RunMode temp = armMotor.getMode();//saves runmode of motor
+        ElapsedTime time = new ElapsedTime(0);
+        time.reset();
 
-        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);//sets mode to use encoder
+        base.setPower(speed);
+        while(time.seconds() < timeRot);
+        base.setPower(0);
 
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);//begins runmode
-
-        double target = armMotor.getCurrentPosition()+( (degrees/360) *COUNTS_PER_MOTOR_REV*DRIVE_GEAR_REDUCTION);//sets target to # of rotations and converts that to motor counts
-
-        armMotor.setTargetPosition((int)target);//sets target
-
-        armMotor.setPower(power);//starts motor at desired power
-
-        while(armMotor.isBusy());//waits until arm is done
-
-        armMotor.setPower(0);//stops motor
-
-        if(Math.abs(armMotor.getCurrentPosition()-target) > COUNTS_PER_MOTOR_REV*0.1){//10% error threshold
-            baseRotateDegree(armMotor, (target-armMotor.getCurrentPosition())/COUNTS_PER_MOTOR_REV/DRIVE_GEAR_REDUCTION*360 , power/2  );//corrects error at slower speed
-        }//error correction
-
-        armMotor.setMode(temp);
-
-    }//rotates motor # of degrees (Rotates in direction of motor)
-
-    public void baseRotateDegreeTo(DcMotor armMotor, double degree, double power){
-
-        double currDeg = armMotor.getCurrentPosition()/COUNTS_PER_MOTOR_REV/DRIVE_GEAR_REDUCTION*360;//converts current encoder pos to a degree angle
-        currDeg = currDeg-(((int)currDeg)/360*360);//removes excess 360's
-
-        baseRotateDegree(armMotor,degree-currDeg,1);//rotates arm to pos
-
-    }//rotates arm to target degree position
-
-    public double getDegree(DcMotor armMotor){
-        return armMotor.getCurrentPosition()/(COUNTS_PER_MOTOR_REV*DRIVE_GEAR_REDUCTION)*360;
-    }//returns current bot arm pos
+    }//rotates base degrees to l/r
 
     public void setGrabPos(){
         // [!] Check all parameters
