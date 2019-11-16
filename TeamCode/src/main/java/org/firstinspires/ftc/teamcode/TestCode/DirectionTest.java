2fc20d66bfee4e844d313e229746affebec9e7ab
Carson Harter <carsonharter@gmail.com>
2019-11-15 19:05:40 -0500

    Update DirectionTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/DirectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/DirectionTest.java
@@ -1,17 +1,17 @@
-package org.firstinspires.ftc.teamcode.FinalBot;
+package org.firstinspires.ftc.teamcode.TestCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.FinalBot.BotArm;
+import org.firstinspires.ftc.teamcode.FinalBot.FinalBot;
 
-import org.firstinspires.ftc.robotcore.internal.tfod.BorderedText;
 
 @Autonomous
 public class DirectionTest extends LinearOpMode {
 
 
     FinalBot bot = new FinalBot(hardwareMap);
-    public BotWheels wheels;
-    public BotArm arm;
+    public BotArm arm = bot.arm;
 
     private double power = .1;
 
@@ -20,24 +20,36 @@ public class DirectionTest extends LinearOpMode {
 
      telemetry.addData("Arm Power", "Positive");
      telemetry.update();
+
+     wait(2000);
+
      arm.baseMotor.setPower(power);
      wait(2000);
      arm.baseMotor.setPower(0);
 
      telemetry.addData("Arm Power", "Negative");
      telemetry.update();
+
+     wait(2000);
+
      arm.baseMotor.setPower(-power);
      wait(2000);
      arm.baseMotor.setPower(0);
 
      telemetry.addData("Wrist Position", "1");
      telemetry.update();
+
+     wait(2000);
+
      arm.wristServo.setPosition(1);
 
      wait(2000);
 
      telemetry.addData("Wrist Position", "0");
      telemetry.update();
+
+     wait(2000);
+
      arm.wristServo.setPosition(0);
 
      wait(2000);
