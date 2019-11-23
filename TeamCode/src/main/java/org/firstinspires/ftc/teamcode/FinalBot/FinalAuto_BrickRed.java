035a44f8b6688ea799c411148a18aa66a6b6fa38
Carson Harter <carsonharter@gmail.com>
2019-11-22 21:01:59 -0500

    Comments added to auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/FinalAuto_BrickRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/FinalAuto_BrickRed.java
@@ -1,3 +1,5 @@
+// [!] START WITH TWO LEFT WHEELS ON WALLS (facing towards closest wall)
+
 package org.firstinspires.ftc.teamcode.FinalBot;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -13,23 +15,27 @@ public class FinalAuto_BrickRed extends LinearOpMode {
         BotArm arm = bot.arm;
         BotWheels wheels = bot.getWheels();
 
+        //sets arm to 18x18x18 box pos
         arm.baseRotateDegree(30, 1);
         arm.baseMotor.setPower(.25);
 
         waitForStart();
 
+        //sets arm back down
         arm.baseMotor.setPower(0);
 
+        //strafes bot to infront of build plate
         bot.move(24, 0);
         bot.move(0, -72);
 
+        //grabs, moves, and places block [!] dysfunctional
         arm.handGrab(true);
-
         bot.placeBlock();
-
         arm.setGrabPos();
 
+        //moves bot onto midline
         bot.move(0, 36);
+        ;
 
 
     }
