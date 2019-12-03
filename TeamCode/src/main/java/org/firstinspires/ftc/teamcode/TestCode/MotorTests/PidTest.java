31009c7221ba001a5083080cee81f54c68b8f2f4
Eric Yung <32915329+Nanomerce@users.noreply.github.com>
2019-12-02 19:32:06 -0500

    Update PidTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/MotorTests/PidTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/MotorTests/PidTest.java
@@ -1,6 +1,28 @@
 package org.firstinspires.ftc.teamcode.TestCode.MotorTests;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+/*
+    The purpose of this program is to simply test PID control
+ */
+
+@Autonomous
 public class PidTest extends LinearOpMode {
+
+    public void runOpMode(){
+
+        DcMotor motor = hardwareMap.get(DcMotor.class, "intakeLeft");
+
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);//start PID
+
+        telemetry.addData("Curr Pos: ",motor.getCurrentPosition());
+
+        
+
+        telemetry.addData("Curr Pos: ",motor.getCurrentPosition());
+
+    }
+
 }
