3255a1c968f495c6a026adfb77f494517403e987
Eric Yung <32915329+Nanomerce@users.noreply.github.com>
2019-12-09 19:22:45 -0500

    Update Midline.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/FinalOpModes/Autonomous/Midline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalBot/FinalOpModes/Autonomous/Midline.java
@@ -14,12 +14,11 @@ public class Midline extends LinearOpMode {
         FinalBot bot = new FinalBot(hardwareMap);
         BotArm arm = bot.arm;
 
-        bot.arm.baseRotateDegree(45, 1);
-        bot.arm.baseMotor.setPower(.25);
+        bot.init();
 
         waitForStart();
 
-        arm.baseMotor.setPower(0);
+        bot.stopInit();
 
         bot.move(0, 18);
 
