546eedf7fee94153ffd9d3d07a050ff9e1151bf5
Eric Yung <32915329+Nanomerce@users.noreply.github.com>
2019-12-09 20:15:39 -0500

    Update InitTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/MotorTests/InitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/MotorTests/InitTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -13,6 +14,11 @@ public class InitTest extends LinearOpMode {
     public void runOpMode(){
         DcMotor base = hardwareMap.get(DcMotor.class, "baseMotor");
         ModernRoboticsI2cRangeSensor range = (ModernRoboticsI2cRangeSensor) hardwareMap.get("armDistance");
+
+        telemetry.addData("Distance: ",range.getDistance(DistanceUnit.CM)+" - "+(8-range.getDistance(DistanceUnit.CM)));
+        telemetry.update();
+        sleep(1000);
+
         base.setPower(1);
 
         while(range.getDistance(DistanceUnit.CM) < 8){
